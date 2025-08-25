23bcf1f1ebd579fc62b8c365ec8b2d763287739f
brian.yu <brian.yu@the-force.com.tw>
2025-08-25 14:10:18 +0800

    fix: use diode writer

--- pkg/log/init.go
+++ pkg/log/init.go
@@ -1,10 +1,11 @@
 package log
 
 import (
-	"os"
-
 	"github.com/chengyu914001/go-common/pkg/sysconst"
 	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/diode"
+	"os"
+	"time"
 )
 
 var defaultLogger zerolog.Logger
@@ -13,13 +14,16 @@ type loggerCtxKeyType struct{}
 
 func init() {
 	switch sysconst.GetEnvMode() {
-	case sysconst.ENV_MODE_LOCAL:
+	case sysconst.ENV_MODE_LOCAL, sysconst.ENV_MODE_DEV:
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	case sysconst.ENV_MODE_DEV:
-		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	case sysconst.ENV_MODE_PROD:
-		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	}
 
-	defaultLogger = zerolog.New(os.Stdout).With().Timestamp().Str("service", sysconst.GetServiceName()).Logger()
+	writer := diode.NewWriter(os.Stdout, 32<<20, 10*time.Millisecond, nil)
+	defaultLogger = zerolog.New(writer).
+		With().
+		Timestamp().
+		Str("service", sysconst.GetServiceName()).
+		Logger()
 }
