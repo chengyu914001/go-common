79981867182fd364597cf151d2b14a103d7c6e4e
BrianYu <lp77734@gmail.com>
2025-08-30 21:08:05 +0800

    refactor: rename fn

--- pkg/core/cpu/limit_run.go
+++ pkg/core/cpu/limit_run.go
@@ -7,13 +7,13 @@ import (
 
 var running = make(chan struct{}, runtime.NumCPU())
 
-func DoLimitRun(ctx context.Context, f func() error) error {
+func DoLimitRun(ctx context.Context, fn func() error) error {
 	select {
 	case running <- struct{}{}:
 		defer func() {
 			<-running
 		}()
-		return f()
+		return fn()
 	case <-ctx.Done():
 		return ctx.Err()
 	}
