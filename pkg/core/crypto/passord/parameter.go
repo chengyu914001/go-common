c87c0f089c1159dbc2134fe9772259257b05e6f9
BrianYu <lp77734@gmail.com>
2025-09-06 19:53:56 +0800

    fix hashed password

---
+++ pkg/core/crypto/passord/parameter.go
@@ -0,0 +1,17 @@
+package passord
+
+import (
+	"sync"
+)
+
+const (
+	memory      uint32 = 65536
+	time        uint32 = 1
+	parallelism uint8  = 1
+	saltLen     uint8  = 16
+	keyLen      uint32 = 32
+)
+
+var (
+	lock = sync.Mutex{}
+)
---
+++ pkg/core/crypto/passord/user.go
@@ -0,0 +1,42 @@
+package passord
+
+import (
+	"crypto/rand"
+	"crypto/subtle"
+	"encoding/base64"
+	"golang.org/x/crypto/argon2"
+)
+
+func HashUserPassword(password string) (string, error) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	salt := make([]byte, saltLen)
+	if _, err := rand.Read(salt); err != nil {
+		return "", err
+	}
+
+	key := argon2.IDKey([]byte(password), salt, time, memory, parallelism, keyLen)
+
+	// len: 22 + 43
+	res := base64.RawStdEncoding.EncodeToString(salt) + base64.RawStdEncoding.EncodeToString(key)
+	return res, nil
+}
+
+func VerifyUserPassword(password string, hashedPassword string) (bool, error) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	salt, err := base64.RawStdEncoding.DecodeString(hashedPassword[:22])
+	if err != nil {
+		return false, err
+	}
+
+	correctKey, err := base64.RawStdEncoding.DecodeString(hashedPassword[22:])
+	if err != nil {
+		return false, err
+	}
+
+	queryKey := argon2.IDKey([]byte(password), salt, time, memory, parallelism, keyLen)
+	return subtle.ConstantTimeCompare(queryKey, correctKey) == 1, nil
+}
